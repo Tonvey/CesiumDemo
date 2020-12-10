82b6f370df861d8545d01ba78ac3453b7d98408a
liangboyan <liangboyanzaz@163.com>
2020-12-10 11:56:59 +0800

    cesium-navigation integration

--- ts/Application.ts
+++ ts/Application.ts
@@ -1,10 +1,24 @@
 import * as Cesium from "cesium";
 export class Application {
   private _containerId: string;
+  private _viewer: Cesium.Viewer;
   constructor(containerId: string) {
     this._containerId = containerId;
   }
   run() {
-    let viewer = new Cesium.Viewer(this._containerId);
+    this._viewer = new Cesium.Viewer(this._containerId);
+    let options = {
+      defaultResetView: Cesium.Rectangle.fromDegrees(71, 3, 90, 14),
+      // Only the compass will show on the map
+      enableCompass: true,
+      enableZoomControls: true,
+      enableDistanceLegend: true,
+      enableCompassOuterRing: true,
+    };
+    this._viewer.extend(Cesium.viewerCesiumNavigationMixin, options);
+  }
+  dispose() {
+    this._viewer.cesiumNavigation.destroy();
+    this._viewer.destroy();
   }
 }
